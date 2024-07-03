2685e99ca990c8e4d9cefd9020cb71ded387037c
betago <betago@outlook.sg>
2024-07-03 15:11:57 +0800

    chore:footer hidden in phone

--- src/app/[lang]/_components/Footer.tsx
+++ src/app/[lang]/_components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+    <div className="absolute bottom-0 left-1/2 hidden -translate-x-1/2 sm:block">
       Power by{" "}
       <a target="blank" href="https://github.com/betgo/Next-push">
         betgo
